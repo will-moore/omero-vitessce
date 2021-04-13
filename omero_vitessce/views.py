99ddcb00dd5b569a7c6bca5ccfbbf8534939a425
William Moore <w.moore@dundee.ac.uk>
2021-04-13 10:43:34 +0100

    Support cells.json for csv and OMERO.table

--- omero_vitessce/views.py
+++ omero_vitessce/views.py
@@ -36,11 +36,7 @@ def index(request, conn=None, **kwargs):
     )
 
 
-@login_required()
-def omero_table(request, file_id, conn=None, **kwargs):
-    """Shows the table columns, allowing user to pick"""
-
-    sample_size = 5
+def get_table_data(conn, file_id, sample_size=None):
     orig_file = conn.getObject("originalfile", file_id)
     file_name = orig_file.getName()
     if file_name.endswith("csv"):
@@ -57,20 +53,24 @@ def omero_table(request, file_id, conn=None, **kwargs):
             columns.append({
                 'name': name, 'type': col_type_str(col_type)
             })
-        for index, row in df.head(sample_size).iterrows():
+        sample = df
+        if sample_size is not None:
+            sample = df.head(sample_size)
+        for index, row in sample.iterrows():
             rows.append(row)
         csvfile.close()
     else:
         r = conn.getSharedResources()
         table = r.openTable(OriginalFileI(file_id), conn.SERVICE_OPTS)
-        table_data = []
+        if sample_size is None:
+            sample_size = table.getNumberOfRows()
         try:
-            res = table.slice(range(len(table_data)), range(sample_size))
             columns = [
                 {'name': col.name,
                 'type': "number" if col.__class__.__name__ == "DoubleColumn" else "string"}
                 for col in table.getHeaders()
             ]
+            res = table.slice(range(len(columns)), range(sample_size))
             rows = [
                 [col.values[row] for col in res.columns]
                 for row in range(0, len(res.rowNumbers))
@@ -79,33 +79,53 @@ def omero_table(request, file_id, conn=None, **kwargs):
         finally:
             table.close()
 
-    index_url = request.build_absolute_uri(reverse('vitessce_index'))
+    return {'rows': rows, 'columns': columns}
 
+
+@login_required()
+def omero_table(request, file_id, conn=None, **kwargs):
+    """Shows the table columns, allowing user to pick"""
+
+    context = get_table_data(conn, file_id, sample_size=5)
+    index_url = request.build_absolute_uri(reverse('vitessce_index'))
+    context['table_id'] = file_id
+    context['index_url'] = index_url
     template = "omero_vitessce/omero_table.html"
-    return render(request, template, {
-        'table_id': file_id,
-        'columns': columns,
-        'rows': rows,
-        'index_url': index_url})
+    return render(request, template, context)
 
 
 @login_required()
 def vitessce_cells(request, fileid, col1, col2, conn=None, **kwargs):
     """
-    Return JSON for vitessce viewer cells.json
+    Return JSON for vitessce viewer cells.json based on a csv or OMERO.table
     http://beta.vitessce.io/docs/data-file-types/index.html#cellsjson
+
+    If there is an ROI or Shape column, load Shape coords and 
+    add 'xy' centre point and 'poly' outline coords.
+
     "cell_1": {
         "mappings": {
             "t-SNE": [-11.0776, 6.0311]
         }
+        "xy": [6824,26313],
+        "poly": [
+            [6668, 26182],
+            [6668, 26296],
+            [6873, 26501],
+            [6932, 26501],
+            [6955, 26478],
+            [6955, 26260],
+            [6838, 26143],
+            [6707, 26143
+        ]
     }
     """
 
-    # Get whole table as JSON (TODO: just query the columns we need)
-    table_dict = _table_query(request, fileid, conn, query="*")
+    # Get whole table as JSON
+    table_data = get_table_data(conn, fileid)
 
-    col_names = table_dict["data"]["columns"]
-    rows = table_dict["data"]["rows"]
+    col_names = [c["name"] for c in table_data["columns"]]
+    rows = table_data["rows"]
 
     col1_index = col_names.index(col1)
     col2_index = col_names.index(col2)
@@ -114,16 +134,16 @@ def vitessce_cells(request, fileid, col1, col2, conn=None, **kwargs):
     if col2_index < 0:
         raise Exception("Column not found: %s" % col2)
 
-    id_column = None
-    for dtype in ["Roi", "Image", "Well"]:
-        if dtype in col_names:
-            id_column = col_names.index(dtype)
-            break
+    # id_column = None
+    # for dtype in ["Roi", "roi_id", "Image", "Well"]:
+    #     if dtype in col_names:
+    #         id_column = col_names.index(dtype)
+    #         break
 
     rv = {}
 
     for count, row in enumerate(rows):
-        row_id = row[id_column]
+        # row_id = row[id_column]
         val1 = row[col1_index]
         val2 = row[col2_index]
         rv["cell_%s" % (count + 1)] = {"mappings": {"UMAP": [val1, val2]}}
